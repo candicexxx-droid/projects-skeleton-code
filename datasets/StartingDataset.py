c64a4e9109b989302dd31fc57c7f7dbccad4d658
Jsgage7 <78756562+Jsgage7@users.noreply.github.com>
2021-05-04 19:58:14 -0700

    updates

--- datasets/StartingDataset.py
+++ datasets/StartingDataset.py
@@ -1,19 +1,52 @@
 import torch
+from torchvision.transforms import ToTensor, Lambda, transforms
+import pandas as pd
+import PIL
+from PIL import Image as Image
+from os import listdir
+import sys
+sys.path.insert(1, '/Users/vinayshukla/Desktop/cassav-project/projects-skeleton-code/train_functions')
+from starting_train import initializationFunction
 
 
-class StartingDataset(torch.utils.data.Dataset):
-    """
-    Dataset that contains 100000 3x224x224 black images (all zeros).
-    """
 
-    def __init__(self):
-        pass
-
-    def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
-
-        return inputs, label
-
+###Loading the dataset & Convert to Tensor
+class StatementDataset(torch.utils.data.Dataset):#inherit from torch.utils.data.Dataset to make our life easier in dealing with Data
+    def __init__(self, statements, labels): #image_id, labels 
+        self.statements = statements  #statement are ids of the images 
+        self.labels = labels 
+        
     def __len__(self):
-        return 10000
+        return len(self.statements)
+    def __getitem__(self, index):  #retrieve items from our dataset 
+        #path needs to be changed 
+        path ='/Users/vinayshukla/Downloads/cassava-leaf-disease-classification/train_images/'
+        trans1 = transforms.ToTensor()        
+        statement = Image.open(path+self.statements[index]) #read specific image
+        statement = trans1(statement)
+        label = self.labels[index]
+        return (statement,label) #return a tuple 
+
+
+def StartingDataset():
+    #change the path
+    image_info = pd.read_csv((r'/Users/vinayshukla/Downloads/cassava-leaf-disease-classification/train.csv'))
+    image_dataset = StatementDataset(image_info.image_id,image_info.label)
+    test_size = 1000
+    valid_size = 1000
+    train_size = len(image_dataset) - 2000
+    train_dataset, test_dataset, valid_dataset = torch.utils.data.random_split(image_dataset, [train_size, test_size, valid_size])
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=True)
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=16, shuffle=True)
+
+    #data for initial model testing 
+    train_test_size = 100
+    train_rest_size = len(train_dataset) - 100
+    train_test, train_rest = torch.utils.data.random_split(train_dataset, [train_test_size, train_rest_size])
+    train_test_loader = torch.utils.data.DataLoader(train_test, batch_size=16, shuffle=True)
+
+    return train_test_loader
+
+initializationFunction(StartingDataset(), None)
--- train_functions/starting_train.py
+++ train_functions/starting_train.py
@@ -2,14 +2,31 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.tensorboard
+from torch.utils.tensorboard import SummaryWriter
+import sys
+sys.path.insert(1, '/Users/vinayshukla/Desktop/cassav-project/projects-skeleton-code/networks')
+from StartingNetwork import CNN
+
+def initializationFunction(training_dataset, val_dataset):
+    hyperparameters = {'epochs': 20, 'batch_size': 16}
+    n_eval = 100
+
+    summary_path = "./log"
+
+    model = CNN(3, 5)
+
+    #Temporary
+
+
+    starting_train(training_dataset, training_dataset, model, hyperparameters, n_eval, summary_path)
+
 
 
 def starting_train(
-    train_dataset, val_dataset, model, hyperparameters, n_eval, summary_path
+    train_dataset, val_dataset, model , hyperparameters, n_eval, summary_path
 ):
     """
     Trains and evaluates a model.
-
     Args:
         train_dataset:   PyTorch dataset containing training data.
         val_dataset:     PyTorch dataset containing validation data.
@@ -23,9 +40,9 @@ def starting_train(
     batch_size, epochs = hyperparameters["batch_size"], hyperparameters["epochs"]
 
     # Initialize dataloaders
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True
-    )
+    #train_loader = torch.utils.data.DataLoader(
+     #   train_dataset, batch_size=batch_size, shuffle=True
+    #)
     val_loader = torch.utils.data.DataLoader(
         val_dataset, batch_size=batch_size, shuffle=True
     )
@@ -40,52 +57,91 @@ def starting_train(
 
     step = 0
     for epoch in range(epochs):
-        print(f"Epoch {epoch + 1} of {epochs}")
+        for batch in train_dataset:
+            images, labels = batch
+            print("Images: " , images.shape)
 
-        # Loop over each batch in the dataset
-        for i, batch in enumerate(train_loader):
-            print(f"\rIteration {i + 1} of {len(train_loader)} ...", end="")
+            #images = images.to(device)
+            #labels = labels.to(device)
 
-            # TODO: Backpropagation and gradient descent
-
+            #probably need to reshape this is its not 784
+            #images = torch.reshape(images, (-1,23040000))
+            outputs = model.forward(images)
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            
             # Periodically evaluate our model + log to Tensorboard
             if step % n_eval == 0:
                 # TODO:
+                print("Here")
                 # Compute training loss and accuracy.
                 # Log the results to Tensorboard.
+                #evaluate(train_dataset, model, loss_fn)
 
                 # TODO:
                 # Compute validation loss and accuracy.
                 # Log the results to Tensorboard.
                 # Don't forget to turn off gradient calculations!
-                evaluate(val_loader, model, loss_fn)
+                #evaluate(val_loader, model, loss_fn)
 
             step += 1
 
-        print()
+        print('Epoch:', epoch, 'Loss:', loss)
 
+    evaluate(train_dataset, model, loss_fn)
 
 def compute_accuracy(outputs, labels):
     """
     Computes the accuracy of a model's predictions.
-
     Example input:
         outputs: [0.7, 0.9, 0.3, 0.2]
         labels:  [1, 1, 0, 1]
-
     Example output:
         0.75
     """
+    n_correct = 0
+    total = 0
+    predictions = torch.argmax(outputs, dim=1)
 
-    n_correct = (torch.round(outputs) == labels).sum().item()
-    n_total = len(outputs)
-    return n_correct / n_total
+
+    n_correct += (predictions == labels).int().sum()
+    total += len(predictions)
+    
+    return (n_correct / total).item()
 
 
-def evaluate(val_loader, model, loss_fn):
+def evaluate(loader, model, loss_fn):
     """
     Computes the loss and accuracy of a model on the validation dataset.
-
-    TODO!
     """
-    pass
+    step = 0
+
+    optimizer = optim.Adam(model.parameters())
+    writer = SummaryWriter(log_dir='./logs')
+    total = 0
+    epochs = 0
+    for epoch in range(loader.batch_size):
+        with torch.no_grad():
+            curr_batch = 0
+            for batch in loader:
+                images, labels = batch
+                
+                #images = images.to(device)
+                #labels = labels.to(device)
+                
+                output = model.forward(images)
+                accuracy = compute_accuracy(output, labels)
+                curr_batch += accuracy
+                loss = loss_fn(output, labels)
+                
+                
+                writer.add_scalar('train_loss', loss, global_step=step)
+                writer.add_scalar('accuracy', accuracy, global_step=step)
+
+                step += 1
+        total+=(curr_batch/loader.batch_size)
+        epochs+=1
+        print("Hi")
+    print("Calculated ", (total/epochs)*100)
