81577748fcaa9a9dd43d476a26b1f14aa058bbdc
candicexxx-droid <76800486+candicexxx-droid@users.noreply.github.com>
2021-07-08 12:41:29 +0800

    add normalization

--- datasets/StartingDataset.py
+++ datasets/StartingDataset.py
@@ -15,6 +15,11 @@ class StatementDataset(torch.utils.data.Dataset):#inherit from torch.utils.data.
     def __init__(self, statements, labels): #image_id, labels 
         self.statements = statements  #statement are ids of the images 
         self.labels = labels 
+        #all pre-trained models expect input images normalized in the same way, i.e. mini-batches of 3-channel RGB images of shape (3 x H x W), where H and W are expected to be at least 224. The images have to be loaded in to a range of [0, 1] and then normalized using mean = [0.485, 0.456, 0.406] and std = [0.229, 0.224, 0.225].
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+        transform1 = transforms.Compose([normalize])
+        self.transform = transform1
         
     def __len__(self):
         return len(self.statements)
@@ -24,6 +29,7 @@ class StatementDataset(torch.utils.data.Dataset):#inherit from torch.utils.data.
         trans1 = transforms.ToTensor()        
         statement = Image.open(path+self.statements[index]) #read specific image
         statement = trans1(statement)
+        statement = self.transform(statement)#normalize
         label = self.labels[index]
         return (statement,label) #return a tuple 
 
@@ -37,6 +43,8 @@ def StartingDataset():
     train_size = len(image_dataset) - 2000
     train_dataset, test_dataset, valid_dataset = torch.utils.data.random_split(image_dataset, [train_size, test_size, valid_size], generator=torch.Generator().manual_seed(42))
 
+    
+)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16, shuffle=True)
     valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=16, shuffle=True)
