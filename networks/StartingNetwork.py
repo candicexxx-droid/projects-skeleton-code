fcff1c2b3bde8364e2d8aee26822f2440eeba9d3
candicexxx-droid <76800486+candicexxx-droid@users.noreply.github.com>
2021-07-09 13:00:30 +0800

    add batch norm

--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -19,79 +19,25 @@ class fc(torch.nn.Module):
         self.fc2 = nn.Linear(512, 256)
         self.bn2 = nn.BatchNorm1d(256)
         self.fc3 = nn.Linear(256, 128)
-        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(128)
         self.fc4 = nn.Linear(128, output_dim)
         
 
     def forward(self, x):
         x = self.flatten(x)
         x = self.fc1(x)
+        x = self.bn1(x)
         x = F.relu(x)
 
         x = self.fc2(x)
+        x = self.bn2(x)
         x = F.relu(x)
 
         x = self.fc3(x)
+        x = self.bn3(x)
         x = F.relu(x)
 
         x = self.fc4(x)
         x = F.softmax(x)
 
-        return x
-
- 
-
-class StartingNetwork(nn.Module): #changed to resnet
-    """
-    Basic CNN to pass the data through
-    """
-    def __init__(self):
-        super().__init__()
-        self.modified_resnet = models.resnet18()
-        # Freeze model weights
-        for param in self.modified_resnet.parameters():
-            param.requires_grad = False
-        
-        print('pretrained weights fixed!')
-
-        # self.resnet = torch.hub.load('pytorch/vision:v0.9.0', 'resnet18', pretrained=True)
-        # self.resnet = torch.nn.Sequential(*(list(self.resnet.children())[:-1])) #//cut off last layer
-        # self.resnet.eval() #//set on eval mode to freeze weights
-
-        # #filter is 5, output channels is 6 (both can be changed)
-        # self.conv1 = nn.Conv2d(input_channels, 6, 5)
-
-        # #the filter dimmensions of the pooling layer (here 2x2, can be changed)
-        # self.pool = nn.MaxPool2d(3, 3)
-
-
-        # #16 output channels, filter still at 5
-        # self.conv2 = nn.Conv2d (6, 12, 5)
-
-        # self.pool2 = nn.MaxPool2d(3, 3)
-
-        #         #16 output channels, filter still at 5
-        # self.conv3 = nn.Conv2d (12, 24, 5)
-
-        # self.pool3 = nn.MaxPool2d(2, 2)
-
-        # self.conv4 = nn.Conv2d(24, 48, 5)
-        # self.pool4 = nn.MaxPool2d(2,2)
-        #commit test
-        # #16 channels, not sure about 4x4
-        self.modified_resnet.fc = fc(512, output_dim=5)
-
-    def forward(self, x):
-
-        # with torch.no_grad(): 
-        #     x = self.resnet(x)#freeze the weight 
-        #    #//use like normal but use no_grad
-
-        # # x = self.pool(F.relu(self.conv1(x)))
-        # # x = self.pool2(F.relu(self.conv2(x)))
-        # # x = self.pool3(F.relu(self.conv3(x)))
-        # # x = self.pool4(F.relu(self.conv4(x)))
-
-        # #keep the fc layers unchanged 
-        x = self.modified_resnet(x)
         return x
